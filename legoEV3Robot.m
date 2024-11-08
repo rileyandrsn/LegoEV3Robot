c835a4328097dd6500083ded9d598bfbb2420e17
Riley Anderson <101540525+rileyandrsn@users.noreply.github.com>
2024-11-07 17:01:28 -0700

    Update legoEV3Robot.m

--- legoEV3Robot.m
+++ legoEV3Robot.m
@@ -1,53 +1,63 @@
+blueHit = false;
+greenHit = false;
+
 global key
 InitKeyboard();
-brick.SetColorMode(2, 4); %initializes color sensor
-while true %keeps program running until manually terminated
-    if(key == 'q') %if q is pressed, move 'C' motor for 0.75 seconds, lifts up arms.
+brick.SetColorMode(2, 4);
+while true
+    if(key == 'q')
         brick.StopAllMotors('Coast');
         brick.MoveMotor('C',15);
         pause(0.75);
         brick.StopAllMotors('Coast');
         break;
     end
-    brick.SetColorMode(2, 4); %redundant code? initializes color sensor
-    color_rgb = brick.ColorRGB(2); %Initializes color_rgb as the number of green detected from the color sensor
-        brick.MoveMotor('A', -15); %moves left 'A' motor at speed of 15, negative value reflects inverted motors on physical lego robot
-        brick.MoveMotor('D', -15); %moves right 'D' motor at speed of 15
-    if (brick.TouchPressed(4)) %if lego touch sensor is pressed (true)
-        brick.StopAllMotors('Coast'); %stops the robot
-        brick.MoveMotor('D', 15); %reverses the robot at speed of 15 on the 'D' motor
-        brick.MoveMotor('A', 15); %reverses the robot at speed of 15 on the 'A' motor
-        pause(1); %reverses robot for 1 second
-        if (brick.UltrasonicDist(1) <= 20) %if after the touch sensor is pressed, the ultrasonic sensor has a value less than or equal to 20, turn left for 1.8 seconds
+    brick.SetColorMode(2, 4);
+    color_rgb = brick.ColorRGB(2); 
+
+        brick.MoveMotor('A', -35);
+        brick.MoveMotor('D', -35);
+    if (brick.TouchPressed(4))
+        brick.StopAllMotors('Coast');
+        brick.MoveMotor('D', 20);
+        brick.MoveMotor('A', 20);
+        pause(1);
+        if (brick.UltrasonicDist(1) <= 35)
         brick.MoveMotor('D', 15);
         brick.MoveMotor('A', -15);
-        pause(1.8);
-        elseif (brick.UltrasonicDist(1) > 20) %else if after touch sensor is pressed, the distance is greater than 20, turn right for 1.8 seconds
+        pause(2.8);
+        elseif (brick.UltrasonicDist(1) > 35)
         brick.MoveMotor('D', -15);
         brick.MoveMotor('A', 15);
-        pause(1.8);
+        pause(2.8);
         end
         brick.StopAllMotors('Coast');
         pause(0.5);
     end
-    if color_rgb(2) < 40 %if the color sensor detects the green value of the color red, stop for one second
+    if color_rgb(1) > 60
         brick.StopAllMotors('Coast');
         pause(1);
-    elseif color_rgb(2) > 125 && color_rgb(2) < 200 % if the color sensor detects the green value of the color green, beep three times
+    elseif color_rgb(3) == 144
+        if(blueHit == false)
         brick.StopAllMotors('Coast');
-        brick.beep();
-        pause(0.3);
-        brick.beep();
-        pause(0.3);
-        brick.beep();
-        pause(0.3);
-    elseif color_rgb(3) > 200 %if the color sensor detects the green value of the color blue, beep twice
+        brick.playTone(1,1,400);
+        pause(0.45);
+        brick.playTone(1,1,400);
+        pause(0.45);
+        blueHit = true;
+        end
+    elseif color_rgb(2) == 59
+        if(greenHit == false)
         brick.StopAllMotors('Coast');
-        brick.beep();
-        pause(0.3);
-        brick.beep();
-        pause(0.3);
+        brick.playTone(1,1,400);
+        pause(0.45);
+        brick.playTone(1,1,400);
+        pause(0.45);
+        brick.playTone(1,1,400);
+        pause(0.45);
+        greenHit = true;
+        end
     end
-        brick.MoveMotor('A', -15); %default move forward
-        brick.MoveMotor('D', -15);
+        brick.MoveMotor('A', -35);
+        brick.MoveMotor('D', -35);
 end
